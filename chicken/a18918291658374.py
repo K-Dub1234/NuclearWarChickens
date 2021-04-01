1630eb815b4b7c4d9eca691423f88f0f71c59b8e
12VM <80974080+12VM@users.noreply.github.com>
2021-04-01 17:26:34 -0600

    Update a18918291658374.py

--- chicken/a18918291658374.py
+++ chicken/a18918291658374.py
@@ -1,9 +1,9 @@
 n = 0
-import random
+import random as r
 meta = []
 while True:
   n += 1
   meta.append(r.randint(1111111111111, 99999999999999))
   f = open("war.chicken." + str(n) + ".wchkn", "a")
-  f.write(meta)
+  r.write(meta)
   
