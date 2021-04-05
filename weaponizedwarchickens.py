5aad982984826c59442a2f262ae428c16ee1fbc5
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-04-04 19:21:39 -0600

    Update weaponizedwarchickens.py

--- weaponizedwarchickens.py
+++ weaponizedwarchickens.py
@@ -1,5 +1,12 @@
 def start():
-  import b18918291658374
+  n = 0
+import random as r
+meta = []
+while True:
+  n += 1
+  meta.append(r.randint(1111111111111, 99999999999999))
+  f = open("war.chicken." + str(n) + ".wchkn", "a")
+  f.write(meta)
 
 def main():
   from PIL import Image
